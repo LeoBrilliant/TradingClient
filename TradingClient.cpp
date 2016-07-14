d9c7210bbdfb2331f220027a2ee88b2160cb7281
LeoBrilliant <LeoBrilliant@gmail.com>
2016-07-14 22:16:17 +0800

    录入报单，貌似在这里做order格式转换不合理。

--- TradingClient.cpp
+++ TradingClient.cpp
@@ -8,6 +8,7 @@
 #include "TradingClient.h"
 #include "UnitTest/TestDataFactory.h"
 #include "Serialization.h"
+#include "zmq.hpp"
 
 using namespace std;
 
@@ -19,11 +20,13 @@ int TradingClient()
 	char server_ip[] = "192.168.31.134";
 	int ret;
 	vector<int> vsock;
-	const int counter = 10;
+	const int counter = 1;
 
 	char buf[1024];
 	int len = 0;
 
+	cout << "Connecting to OpenTS" << endl;
+	//sockets to OpenTS
 	for(int i = 0; i < counter; ++i)
 	{
 		sockfd = -1;
@@ -47,6 +50,85 @@ int TradingClient()
 		}
 	}
 
+	// socket to receive orders
+	//  Prepare our context and socket
+	zmq::context_t context (1);
+	zmq::socket_t socket (context, ZMQ_REP);
+	socket.bind ("tcp://*:5556");
+
+	cout << "Waiting for New Orders\n" << endl ;
+	int msgCounter = 0;
+	while (true) {
+		zmq::message_t request;
+
+		memset(buf, 0, 1024);
+
+		//  Wait for next request from client
+		socket.recv (&request);
+		std::cout << "Received Data: " << (char*)(request.data()) << std::endl;
+
+		//  Do some 'work'
+		//Order * op = GenOrder();
+		//op->SetDirection(DirectionType::SELL);
+		//StringType strOrder = SerializeOrder(op);
+		//sprintf(buf, "%d\t%s: %d", ++msgCounter, strOrder.c_str(), vsock[0]);
+		//sprintf(buf, "%s", strOrder.c_str());
+
+		//sleep(1);
+		//sprintf(buf, "%s: %d", (char*)(request.data()), sockfd);
+		//sprintf(buf, "%d\t%s: %d", ++msgCounter, "Hello", sockfd);
+
+		Json::Reader reader(Json::Features::strictMode());
+		Json::Value input;
+		Json::Value jsonOrder;
+		Json::Value jsonInst;
+		if(reader.parse((char *)(request.data()), input))
+		{
+			jsonInst["InstructionID"] = "";
+			jsonInst["ClientID"] = atoi(input["clientID"].asCString());
+			if(input["instType"] == "LimitPriceOrder")
+				jsonInst["InstType"] = InstructionType::LIMITPRICEORDER ;
+
+			jsonOrder["Instruction"] = jsonInst;
+
+			jsonOrder["InstrumentID"] = "TestInstrument";
+
+			if(input["direction"] == "BUY")
+				jsonOrder["Direction"] = DirectionType::BUY;
+			jsonOrder["OffsetFlag"] = OffsetType::OPEN;
+			jsonOrder["OrderPrice"] = atof(input["orderPrice"].asCString());
+			jsonOrder["Volume"] = atoi(input["volume"].asCString());
+			jsonOrder["OrderID"] = "";
+			jsonOrder["LocalOrderID"] = msgCounter;
+			jsonOrder["VolumeLeft"] = "";
+			jsonOrder["OrderStatus"] = "";
+
+			StyledWriter fast_writer;
+			StringType strOrder = fast_writer.write(jsonOrder);
+			memcpy(buf, strOrder.c_str(), strOrder.size());
+			len = strlen(buf);
+			++msgCounter;
+			ret = write(sockfd, buf, len);
+			cout << "write " << msgCounter << "(" << ret << ") : " <<  buf << endl;
+			//  Send reply back to client
+			zmq::message_t reply (12);
+			memcpy (reply.data (), "Order Sended", 12);
+			socket.send (reply);
+		}
+		else
+		{
+			cout << "Invalid Order" << endl;
+			//  Send reply back to client
+			zmq::message_t reply (13);
+			memcpy (reply.data (), "Invalid Order", 13);
+			socket.send (reply);
+		}
+		//sprintf(buf, "%s", (char*)(request.data()));
+
+
+	}
+
+	/*
 	for(int i = 0; i < counter; ++i)
 	{
 		Order * op = GenOrder();
@@ -57,7 +139,7 @@ int TradingClient()
 		write(vsock[i], buf, len);
 		printf("write: %s\n", buf);
 	}
-
+	*/
 	printf("write finished, sleep 1s\n");
 	sleep(1);
 
